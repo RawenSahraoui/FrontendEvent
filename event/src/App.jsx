9fdcc5028f2f0ba0305e202d6d77b1317967e71e
RawenSahraoui <154666302+RawenSahraoui@users.noreply.github.com>
2024-05-27 01:46:11 +0200

    frontend

--- event/src/App.jsx
+++ event/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <Route path="/agenda" element={<Agenda />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </Router>
       <Footer />
--- event/src/components/registration/AdminDashboard.jsx
+++ event/src/components/registration/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './AdminDashboard.css';
 
 const AdminDashboard = () => {
   const [registrations, setRegistrations] = useState([]);
